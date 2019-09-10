63d99a2463912aeca77de763fa2f87a1ed212674
bdelanghe <bdelanghe@gmail.com>
2019-09-10 13:00:29 -0400

    merge issues

--- cmd_tictactoe.py
+++ cmd_tictactoe.py
@@ -31,14 +31,97 @@ CursesCords = namedtuple('Cords', ['y', 'x'])
 
 class CmdMode(Cmd):
     """cmd wrapper for interactions"""
-    intro = f.renderText('TIC TAC TOE')
-    prompt = '(play)'
-    file = None
+    doc_leader = """
+    
+Play a simple game inside python's cmd module. The board coordinates are:
+
+              0 1 2 x
+            0| | | |
+            1| | | |
+            2| | | |
+            y
+            
+    """
+    doc_header = 'everything you can do (more info: type ?<cmd>)'
+    ruler = '~'
+
+    # noinspection PyUnusedLocal
+    def do_bye(self, line):
+        """all good things come to an end"""
+        self._end = True
+        self._print_bye()
+        return True
+
+    # noinspection PyUnusedLocal
+    def do_new_game(self, line):
+        """feel like rage quitting?"""
+        self.session.new_game()
+        self._print_board()
+
+    def do_name(self, line):
+        """give the current player a name"""
+        if line == '':
+            print('enter a name')
+            self._do_last = True
+        else:
+            self.session.current_game.current_player.name = line
+            self._do_last = False
+
+    def do_move(self, line):
+        """make a move, format must be: 'move y x''"""
+        if line == '':
+            self._do_last = True
+        elif line == 'help':
+            self._do_last = False
+            self.do_help('')
+        elif line == 'stop':
+            self._do_last = False
+        else:
+            self._do_last = True
+            try:
+                y, x = [int(s) for s in line.split()]
+                sqr = Square(y=y, x=x)
+                self.session.current_game.square_input(sqr)
+            except ValueError:
+                if self._do_last is False:
+                    print('format must be: "move y x"')
+                else:
+                    print('format must be: "y x" or type "stop"')
+
+    # noinspection PyUnusedLocal
+    def do_score(self, line):
+        """I wonder who's winning"""
+        self._print_scores()
 
     def __init__(self):
         super().__init__()
-        self.session = Session
-        self.player = 0
+        self.session = Session()
+        self.players = self.session.players
+        self._end = False
+        self._do_last = False
+
+    @property
+    def prompt(self):
+        """add the current player to prompt"""
+        p = str(self.session.current_game.current_player.name)
+        if self._do_last is True and self.last_simple is not None:
+            p += f': {self.last_simple}'
+        return f'({p})'
+
+    @property
+    def last_simple(self):
+        """there is no try only do"""
+        if self._do_last is True:
+            trim = self.lastcmd.split()[0]
+            try:
+                getattr(self, 'do_' + trim)
+                return trim
+            except AttributeError:
+                pass
+
+    def emptyline(self):
+        """rewriting so it's more fun"""
+        print("Why so quiet?")
 
     def default(self, line):
         """making errors more fun"""
@@ -124,32 +207,10 @@ class Session:
         self.current_game: Optional[Game] = None
         self.play_count: int = 0
 
-    def _new_game(self) -> None:
-        """create a new game"""
-        if self.current_game is not None:
-            self.play_count += 1
-            del self.current_game
-        self.current_game = Game(self)
-
-    def _print_scores(self) -> None:
-        """display the current player scores to console"""
-        scores = ''
-        for player in self.players.values():
-            scores += f'Player {player.number + 1}: {player.score}      '
-        print(scores + '\n')
-
-    @staticmethod
-    def _print_exit_message() -> None:
-        """saying goodbye is always hard"""
-        print('Thanks for playing!!')
-
-    def play(self):
-        """keep playing games until quit"""
-        while self.quit is False:
-            self._print_scores()
-            self._new_game()
-            self.current_game.play()
-        self._print_exit_message()
+    @property
+    def first_move(self) -> int:
+        """which player gets to go first"""
+        return self.play_count % 2
 
 
 class Game:
@@ -297,235 +358,9 @@ class Moves:
         return False
 
 
-class CursesSession(Session):
-    """a class extension to include curses"""
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.quit_char = '`'
-        self.window = Window(self.play())
-
-    def play(self) -> None:
-        """while loop for playing in curses"""
-        curses.curs_set(0)
-
-        while self.quit is False:
-            self._new_game()
-            self.current_game.play()
-
-
-# class AiPlayer(Player):
-#     """smart player extension"""
-#
-#     # todo add AI class
-#     def __init__(self, number) -> None:
-#         super().__init__(number)
-
-
-class CursesGame(Game):
-    """extended Game class for Curses"""
-
-    def __init__(self, session: CursesSession) -> None:
-        super().__init__(session)
-        self.session = session
-        self.board: BoardLib = BoardLib(self.session.window)
-
-    def play(self) -> None:
-        """display loop for curses"""
-        # fixme after simplifying play
-        # self.stdscr.clear()
-        # self.stdscr.refresh()
-        #
-        # self.draw_board()
-        #
-        # while self.end is False:
-        #     # todo make one call
-        #     move = self.input()
-        #
-        #     stdscr.refresh()
-
-    def _draw_board(self) -> None:
-        """draw board in curses window"""
-        str_list = self.board.board_grid
-        self.session.window.draw_centered(str_list)
-
-    def _curses_input(self) -> None:
-        """overwrite game func to allow for quiting"""
-        char: str = self.session.window.get_input()
-        if char == self.session.quit_char:
-            self.end = True
-            self.session.quit = True
-        else:
-            self._input(char)
-
-
-# class BoardLib:
-#     """extended library object for curses"""
-#
-#     def __init__(self, window: Window = None):
-#         self._window = window
-#         self.n_by: int = 3
-#         self.scale: float = 2 / 3
-#         self.line_fill: str = u'\u2588'
-#         self.cross_fill: str = '?'
-#         self.nought_fill: str = '@'
-#         self.square_map: dict = {}
-#
-#     @property
-#     def _window(self) -> Window:
-#         """Current curses window for obj"""
-#         return self._window
-#
-#     @_window.setter
-#     def _window(self, value: Optional[Window]) -> None:
-#         """allow for no window object for debugging"""
-#         if value is not None:
-#             self._window = value
-#
-#     @property
-#     def _square_size(self) -> Size:
-#         """find the pixel box size for each square"""
-#         sqr_y = int((self._window.short_side * self.scale) / self.n_by)
-#         sqr_x = sqr_y * 2
-#         return Size(y=sqr_y, x=sqr_x)
-#
-#     @property
-#     def board_grid(self) -> List[str]:
-#         """creating a string for printing the TicTacToe grid"""
-#         # todo: rewrite to just give cords of lines
-#         lines = []
-#         size = self._square_size
-#         self.square_map = {}
-#
-#         for row in range(1, (size.y * self.n_by) + self.n_by):
-#             line = ''
-#             for col in range(1, (size.y * self.n_by) + self.n_by):
-#                 y = int(row / self.n_by)
-#                 x = int(col / self.n_by)
-#                 sqr_cord = Square(y=y, x=x)
-#                 curse_cord = CursesCords(y=row, x=col)
-#                 if row % (size.y + 1) == 0:
-#                     line += self.line_fill * 2
-#                 elif col % (size.y + 1) == 0:
-#                     line += self.line_fill * 2
-#                 else:
-#                     if sqr_cord not in self.square_map:
-#                         self.square_map[sqr_cord] = []
-#                     self.square_map[sqr_cord].append(curse_cord)
-#             lines.append(line)
-#
-#         return lines
-#
-#     @staticmethod
-#     def invert(string) -> str:
-#         """invert the characters of a square"""
-#         fill = max([c for c in string])
-#         return str([' ' if fill else fill for _ in string])
-#
-#     @property
-#     def cross(self) -> str:
-#         """create a string for a cross"""
-#         string = ''
-#         sqr = self._square_size
-#
-#         for row in range(sqr.y):
-#             for col in range(sqr.x):
-#                 if col == row * 2 or col == (sqr.x - 1) - (row * 2):
-#                     string += self.cross_fill
-#                 else:
-#                     string += ' '
-#         return string
-#
-#     @property
-#     def nought(self) -> str:
-#         """create a string for a nought"""
-#         string = ''
-#         sqr = self._square_size
-#
-#         # Needed to add a boarder to get it to look right
-#         border = 1 / 2
-#         radius = sqr.y / 2 - border
-#         for row in range(sqr.y):
-#             for col in range(sqr.y):
-#                 dist = math.sqrt((row - radius) * (row - radius) +
-#                                  (col - radius) * (col - radius)) + 1
-#                 if radius - border < dist < radius + border:
-#                     string += self.nought_fill * 2
-#                 else:
-#
-#                     string += ' ' * 2
-#         return string
-
-
-# def title_screen(stdscr: curses.initscr) -> None:
-#     """a fun title screen"""
-#     # todo turn this into an object?
-#     # Set properties
-#     curses.curs_set(0)
-#     stdscr.nodelay(True)
-#
-#     # Get key input
-#     k = stdscr.getch()
-#
-#     # Get window params
-#     win_h, win_w = stdscr.getmaxyx()
-#
-#     def wipe_text(text: str) -> int:
-#         """load multi-line text from left to right"""
-#         lines = text.split("\n")
-#
-#         # Get dimensions for text
-#         x_len = len(lines[0])
-#         y_len = len(lines)
-#
-#         # Find center for text
-#         x = int((win_w - x_len) / 2)
-#         y = y_start = int((win_h - y_len) / 2)
-#
-#         for i in range(x_len):
-#             y = y_start
-#             for line in lines[:-1]:
-#                 stdscr.addstr(y, x, line[i])
-#                 y += 1
-#             stdscr.refresh()
-#             time.sleep(.05)
-#             x += 1
-#         return y
-#
-#     def load_title() -> None:
-#         """add title to window"""
-#         title = f.renderText('TIC TAC TOE')
-#         sub_title = 'produced by: robert delanghe studio'
-#         any_key = 'press any key to continue'
-#
-#         y: int = wipe_text(title)
-#
-#         stdscr.addstr(y, int((win_w - len(sub_title)) / 2), sub_title)
-#         stdscr.refresh()
-#
-#         time.sleep(1)
-#
-#         stdscr.addstr(win_h - 1, win_w - len(any_key) - 2, any_key, curses.A_BLINK + curses.A_DIM)
-#
-#         stdscr.refresh()
-#
-#     load_title()
-#
-#     while k == -1:
-#         k = stdscr.getch()
-
-
-# def play() -> None:
-#     """lets play tic tac toe"""
-#     sess = Session()
-#     sess.play()
-
-
 def main() -> None:
     """want to play a game of tic tac toe?"""
-    # curses.wrapper(title_screen)
-    play()
-    # CmdMode().cmdloop()
+    CmdMode().cmdloop()
 
 
 if __name__ == "__main__":
